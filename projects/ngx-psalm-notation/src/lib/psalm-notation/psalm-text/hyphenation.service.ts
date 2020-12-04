02dc7aff462744a6e3ca9e48ec5dc36d96f2319c
Espesen <esa.toivola@gmail.com>
2020-12-04 22:00:14 +0200

    Hyphenation exceptions

--- projects/ngx-psalm-notation/src/lib/psalm-notation/psalm-text/hyphenation.service.ts
+++ projects/ngx-psalm-notation/src/lib/psalm-notation/psalm-text/hyphenation.service.ts
@@ -16,11 +16,32 @@ export class HyphenationService {
 
   hyphenate(text: string): string[] {
 
+    type exception = { syllable: string, replace: string[] };
+    const exceptions: exception[] = [
+      { syllable: 'älä', replace: ['ä', 'lä']}
+    ];
+
+    const getFindFn = (syllable: string) => (exc: exception) => !!syllable.match(new RegExp(exc.syllable, 'i'));
+    const replaceFn = (exc: exception, originalSyllable: string): string[] => {
+      const isUpperCase = !!originalSyllable.match(/[A-ZÅÄÖ]/);
+      const addSpaceToLast = (syll: string, i: number, arr: string[]) => i === arr.length - 1 ? syll + ' ' : syll;
+      return isUpperCase ?
+        exc.replace.map((syll, i, arr) => i === 0 ? syll.charAt(0).toUpperCase() + syll.slice(1) : syll)
+          .map(addSpaceToLast) :
+        exc.replace
+          .map(addSpaceToLast);
+    };
+    const reduceFn = (arr: string[], syllable: string): string[] => exceptions
+      .find(getFindFn(syllable)) ?
+        arr.concat(...replaceFn(exceptions.find(getFindFn(syllable)), syllable)) :
+        arr.concat(syllable);
+
     return hypher.hyphenate(text)
       .reduce((acc, curr) => acc.concat(...curr.replace(/\s([\S=])/g, '*$1').split('*')
         .map((item, index, array) => index === 0 && array.length > 1 ? item + ' ' : item)), [])
       // remove em dashes
       .filter(item => item !== '–')
+      .reduce(reduceFn, [])
       .map((item, index, array) => item
         .match(/[^\s]$/) && index < array.length - 1 ? item + '-' : item.replace(/\s$/, ''))
       .map(item => item.replace('=-', '='));
